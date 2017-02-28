94d516ff59ddff52a6923df128364fff374b9dbe
manuelmolano <manuelmolanomazon@gmail.com>
2017-02-28 12:52:03 +0100

    pdf_resolution.m: updated the code

--- pdf_resolution.m
+++ pdf_resolution.m
@@ -4,15 +4,19 @@ if nargin==0
     resolution = 200;
 end
 current_folder = cd;
-cd('C:\Program Files\gs\gs9.19\bin')
+cd('C:\Program Files\gs\gs9.20\bin')
 [file_name, data_main_folder] = uigetfile('*.pdf');
 file_name = file_name(1:end-4);
 command = ['gswin64c -sDEVICE=pngalpha -dQUIET -dBATCH -dNOPAUSE -sOutputFile=' data_main_folder file_name 'DEL%02d.png -r' num2str(resolution) ' ' data_main_folder file_name '.pdf'];
 system(command);
-command = ['convert ' data_main_folder file_name '*DEL*.png '  data_main_folder file_name 'DEL%02d.pdf'];
+disp('png files created')
+cd('C:\Program Files\ImageMagick-7.0.5-Q16')
+command =  ['magick ' data_main_folder file_name '*DEL*.png +adjoin ' data_main_folder file_name 'DEL%d.pdf'];
 system(command)
+disp('png files converted to pdf')
 command = ['pdftk ' data_main_folder file_name '*DEL*.pdf cat output ' data_main_folder file_name '_res' num2str(resolution) '.pdf'];
 system(command);
+disp('pdf files concatenated')
 cd(data_main_folder)
 delete('*DEL*')
 cd(current_folder)
