d543146a8303cbe1ad24831d809b2cd324933ffb
manuelmolano <manuelmolanomazon@gmail.com>
2017-09-13 10:05:19 +0200

    small change (%02d) so pages are in order

--- looking_for_words_in_codes.m
+++ looking_for_words_in_codes.m
@@ -1,15 +1,15 @@
 function looking_for_words_in_codes()
-carpeta = 'C:\Users\molano\ETIC\';
+carpeta = 'C:\Users\molano\blender_codes\analysisCode_matlab\';
 a = dir([carpeta '*.m']);
 
 
-funcion = 'other_pixels';
+funcion = '_reversed';
 funcion_shown = 0;
 for ind2=1:numel(a)
     candidato = a(ind2).name;
     [fileID,~] = fopen([carpeta candidato]);
     d = textscan(fileID,'%s','Delimiter','\n');
-    aux = strfind(d{1},funcion(1:end-2));
+    aux = strfind(d{1},funcion);
     aux_comment = strfind(d{1},'%');
     aux = cellfun(@isempty,aux);
     aux_comment = cellfun(@isempty,aux_comment);
